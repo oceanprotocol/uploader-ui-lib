3a5e93a47fe9ad86f5b9bb4ea16b69322842ab81
EnzoVezzaro <enzo-vezzaro@live.it>
2023-05-09 09:44:31 -0400

    renamed project and minor changes to configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,29 @@
-import nodeResolve from '@rollup/plugin-node-resolve'
-import typescript from 'rollup-plugin-typescript2'
-import styles from 'rollup-plugin-styles'
-import image from '@rollup/plugin-image'
-import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
-import packageJson from './package.json'
+import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import styles from 'rollup-plugin-styles';
+import image from '@rollup/plugin-image';
+import {sizeSnapshot} from 'rollup-plugin-size-snapshot';
+import packageJson from './package.json';
 
 export default [
   {
     input: 'src/index.ts',
-    external: [...Object.keys(packageJson.devDependencies)],
-    output: [
-      { file: packageJson.main, format: 'cjs', sourcemap: true },
-      { file: packageJson.module, format: 'es', sourcemap: true }
+    external: [
+      ...Object.keys(packageJson.devDependencies)
     ],
-    plugins: [nodeResolve(), typescript(), styles(), image(), sizeSnapshot()],
+    output: [
+      {file: packageJson.main, format: 'cjs', sourcemap: true},
+      {file: packageJson.module, format: 'es', sourcemap: true}
+    ],
+    plugins: [
+      nodeResolve(),
+      typescript(),
+      styles(),
+      image(),
+      sizeSnapshot()
+    ],
     watch: {
       clearScreen: false
     }
   }
-]
+];
