18c0f98d7d4fec3dc449f44add8a5c5ea4806b41
EnzoVezzaro <enzo-vezzaro@live.it>
2023-05-11 09:57:05 -0400

    init storybook

---
+++ src/stories/DBSUploader.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StoryFn, Meta } from "@storybook/react";
+import DBSUploader from "../components/DBSUploader";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "ReactComponentLibrary/DBSUploader",
+  component: DBSUploader,
+} as Meta<typeof DBSUploader>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: StoryFn<typeof DBSUploader> = (args) => <DBSUploader {...args} />;
+
+export const dbsuploader = Template.bind({});
+dbsuploader.args = {
+  children: "Click me!",
+};
