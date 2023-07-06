0a47b65e32d541beed1f24d51cf468438fa0a1f1
EnzoVezzaro <enzo-vezzaro@live.it>
2023-07-06 14:44:13 -0400

    added wagmi v0.12.12

--- src/components/DBSUploader/index.tsx
+++ src/components/DBSUploader/index.tsx
@@ -1,6 +1,8 @@
+require('dotenv').config()
 import React from 'react'
-import { WagmiConfig, createConfig, mainnet } from 'wagmi'
-import { createPublicClient, http } from 'viem'
+import { WagmiConfig, createClient } from 'wagmi'
+import { mainnet, polygon, goerli, polygonMumbai } from 'wagmi/chains'
+import { ConnectKitProvider, getDefaultClient } from 'connectkit'
 
 import '../../stylesGlobal/styles.css'
 import './index.module.css'
@@ -9,13 +11,31 @@ import TabsFile from '../TabsFile'
 // TODO: connect to remote DBSclient
 // import DBSClient from 'dbs/src/index';
 
-const config = createConfig({
-  autoConnect: true,
-  publicClient: createPublicClient({
-    chain: mainnet,
-    transport: http()
-  }),
-})
+// Wagmi client
+export const wagmiClient = createClient(
+  getDefaultClient({
+    appName: 'Ocean Market',
+    infuraId: process.env.PUBLIC_INFURA_PROJECT_ID,
+    // TODO: mapping between appConfig.chainIdsSupported and wagmi chainId
+    chains: [mainnet, polygon, goerli, polygonMumbai],
+    walletConnectProjectId: process.env.PUBLIC_WALLETCONNECT_PROJECT_ID || ''
+  })
+)
+
+// ConnectKit CSS overrides
+// https://docs.family.co/connectkit/theming#theme-variables
+export const connectKitTheme = {
+  '--ck-font-family': 'var(--font-family-base)',
+  '--ck-border-radius': 'var(--border-radius)',
+  '--ck-overlay-background': 'var(--background-body-transparent)',
+  '--ck-modal-box-shadow': '0 0 20px 20px var(--box-shadow-color)',
+  '--ck-body-background': 'var(--background-body)',
+  '--ck-body-color': 'var(--font-color-text)',
+  '--ck-primary-button-border-radius': 'var(--border-radius)',
+  '--ck-primary-button-color': 'var(--font-color-heading)',
+  '--ck-primary-button-background': 'var(--background-content)',
+  '--ck-secondary-button-border-radius': 'var(--border-radius)'
+}
 
 type dbs_setting = {
   title: string,
@@ -51,10 +71,15 @@ const DBSUploader = ({ dbs_settings }: Props) => {
   */
   
   return (
-    <WagmiConfig config={config}>
+    <WagmiConfig client={wagmiClient}>
+        <ConnectKitProvider
+          options={{ initialChainId: 0 }}
+          customTheme={connectKitTheme}
+        >
       <div className="DBSUploader">  
         <TabsFile items={dbs_settings as any} />
       </div>
+      </ConnectKitProvider>
     </WagmiConfig>
   )
 }
