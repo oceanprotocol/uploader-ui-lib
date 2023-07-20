229201a0df4fb7f163d5ef12dbb52ce92206bf7f
EnzoVezzaro <enzo-vezzaro@live.it>
2023-07-20 12:14:50 -0400

    refactor dbs settings fetching

--- src/components/DBSUploader/index.tsx
+++ src/components/DBSUploader/index.tsx
@@ -10,16 +10,6 @@ import TabsFile from '../TabsFile'
 
 import {DBSClient} from '@oceanprotocol/dbs';
 
-// Wagmi client
-export const wagmiClient = createClient(
-  getDefaultClient({
-    appName: 'Ocean DBS UI',
-    infuraId: process.env.PUBLIC_INFURA_PROJECT_ID,
-    chains: [mainnet, polygon, goerli, polygonMumbai],
-    walletConnectProjectId: process.env.PUBLIC_WALLETCONNECT_PROJECT_ID || ''
-  })
-)
-
 // ConnectKit CSS overrides
 // https://docs.family.co/connectkit/theming#theme-variables
 export const connectKitTheme = {
@@ -32,7 +22,8 @@ export const connectKitTheme = {
   '--ck-primary-button-border-radius': 'var(--border-radius)',
   '--ck-primary-button-color': 'var(--font-color-heading)',
   '--ck-primary-button-background': 'var(--background-content)',
-  '--ck-secondary-button-border-radius': 'var(--border-radius)'
+  '--ck-secondary-button-border-radius': 'var(--border-radius)',
+  '--ck-body-color-muted': 'var(--font-color-text)'
 }
 
 type acceptedTokens = {
@@ -41,7 +32,7 @@ type acceptedTokens = {
 }
 
 type payment = {
-  chainId: number
+  chainId: string
   acceptedTokens: acceptedTokens[]
 }
 
@@ -51,14 +42,30 @@ export type dbs_setting = {
   payment: payment[]
 }
 
-const DBSUploader = () => {
-  // const { data: signer } = useSigner()
+type dbsParams = {
+  dbs_url: string
+  infuraId: string
+  walletConnectProjectId: string
+}
+
+const DBSUploader = ({ dbs_url, infuraId, walletConnectProjectId }: dbsParams) => {
   const [ DBSsettings, setDBSsettings ] = React.useState([])
-  const [ DBSnetworks, setDBSnetworks ] = React.useState([])
+  const [ networksChainIds, setNetworksChainIds ] = React.useState([])
+  const [ loading, setLoading ] = React.useState(true)
   // Initialize the DBSClient with the API base URL
-  const client = new DBSClient(process.env.DBS_URL || ''); 
+  const client = new DBSClient(dbs_url || '');
+  // Initialize the Wagmi client
+  const wagmiClient = createClient(
+    getDefaultClient({
+      appName: 'Ocean DBS UI',
+      infuraId: infuraId,
+      chains: [mainnet, polygon, goerli, polygonMumbai],
+      walletConnectProjectId: walletConnectProjectId || ''
+    })
+  )
   // Fetch storage info
   useEffect(() => {
+    // Fetch storage info from DBS endpoint
     const getStorageInfo = async () => {
       try {
         const storageInfo = await client.getStorageInfo();
@@ -68,30 +75,27 @@ const DBSUploader = () => {
         return error;
       }
     }
-
-    const getChainIds = (data: any) => {
+    // Get chainIds from DBS settings
+    const getChainIds = (DBSsettings: any) => {
       const chainIds = new Set();
-      data.forEach((item: dbs_setting) => {
+      DBSsettings.forEach((item: dbs_setting) => {
         (item.payment || []).forEach((paymentItem: payment) => {
-          chainIds.add(paymentItem.chainId);
+          chainIds.add(parseInt(paymentItem.chainId));
         });
       });
       return Array.from(chainIds);
     }
     // Fetch storage info
-    getStorageInfo().then((storageInfo) => {
+    getStorageInfo().then((storageInfo: any) => {
+      const chainIds: any = getChainIds(storageInfo);
       // TODO: fix error on DBS.js when endpoint is empty / unavailable / wrong
-      setDBSsettings(storageInfo as any)
-      console.log(storageInfo);
-      setDBSnetworks(getChainIds(storageInfo) as any);
+      setDBSsettings(storageInfo)
+      setNetworksChainIds(chainIds)
+      setLoading(false)
     }).catch((err) => {
       console.log(err);
     })
   }, [])
-
-  useEffect(() => {
-    console.log(DBSnetworks);
-  }, [DBSnetworks])
   
   return (
     <WagmiConfig client={wagmiClient}>
@@ -100,7 +104,15 @@ const DBSUploader = () => {
           customTheme={connectKitTheme}
         >
       <div className="DBSUploader">  
-        <TabsFile items={DBSsettings as dbs_setting[]} />
+        { DBSsettings.length === 0 && loading && <div>Loading...</div> }
+        { DBSsettings.length > 0 && 
+          <TabsFile items={DBSsettings as dbs_setting[]} availableNetworks={networksChainIds} />
+        }
+        {
+          DBSsettings.length === 0 && !loading && (
+            <p>DBS Not available</p>
+          )
+        }
       </div>
       </ConnectKitProvider>
     </WagmiConfig>
