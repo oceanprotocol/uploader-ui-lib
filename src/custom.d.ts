e2e738fcd5cc0c29477a189dda1173609d8e4f05
EnzoVezzaro <enzo-vezzaro@live.it>
2023-08-29 08:46:27 -0400

    remove scss declarations as there's not used

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -32,13 +32,3 @@ declare module '*.module.css' {
   const classes: { readonly [key: string]: string }
   export default classes
 }
-
-declare module '*.module.scss' {
-  const classes: { readonly [key: string]: string }
-  export default classes
-}
-
-declare module '*.module.sass' {
-  const classes: { readonly [key: string]: string }
-  export default classes
-}
