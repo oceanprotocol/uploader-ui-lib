67537b60ed8643fbbd0ee6167c7539288b4ac411
EnzoVezzaro <enzo-vezzaro@live.it>
2023-08-30 08:48:51 -0400

    update rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,45 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import image from '@rollup/plugin-image';
+import svgr from '@svgr/rollup'
 import packageJson from './package.json';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from 'rollup-plugin-postcss';
 import {terser} from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
 import json from "@rollup/plugin-json";
+import dts from "rollup-plugin-dts";
+import postcssImport from 'postcss-import';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default [
   {
     input: 'src/index.ts',
-    external: [
-      ...Object.keys(packageJson.devDependencies)
-    ],
     output: [
       {file: packageJson.main, format: 'cjs', sourcemap: true, inlineDynamicImports: true},
       {file: packageJson.module, format: 'es', sourcemap: true, inlineDynamicImports: true}
     ],
     plugins: [
       peerDepsExternal(),
-      nodeResolve(),
+      commonjs(),
+      nodePolyfills(),
       typescript({ useTsconfigDeclarationDir: true }),
       json(),
       postcss({
         sourceMap: true,
         extract: true,
-        minimize: true
+        minimize: true,
+        plugins: [postcssImport()]
       }),
-      image(),
-      terser(),
-      commonjs()
+      svgr(),
+      terser()
     ],
     watch: {
       clearScreen: false
     }
+  },
+  {
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports 
   }
 ];
