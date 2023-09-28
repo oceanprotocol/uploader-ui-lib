a473e033ff0eaa0f4a79f34936c85c8762c87cdf
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-09-28 19:32:50 +0300

    updating options

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -14,5 +14,14 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /date$/
     }
+  },
+  options: {
+    showRoots: false, // Hides the root in the sidebar
+    showPanel: false, // Hides the addon panel at the bottom
+  },
+  toolbars: {
+    previewActions: {
+      hidden: true, // Hides the top toolbar (with the 'show code' button, etc.)
+    },
   }
 }
