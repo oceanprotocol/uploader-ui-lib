4a13511e99b24525e5ac6569ca38e423aa07f7c8
Enzo Vezzaro <enzo-vezzaro@live.it>
2023-10-04 13:34:21 +0200

    tested commands

--- rollup.config.js
+++ rollup.config.js
@@ -1,52 +1,45 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import {dts} from "rollup-plugin-dts";
-import terser from '@rollup/plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import svgr from '@svgr/rollup'
+import packageJson from './package.json';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import postcss from 'rollup-plugin-postcss';
-import postcssImport from 'postcss-import';
-import svgr from '@svgr/rollup'
+import terser from '@rollup/plugin-terser';
+import commonjs from '@rollup/plugin-commonjs';
 import json from "@rollup/plugin-json";
-
-const packageJson = require("./package.json");
+import {dts} from "rollup-plugin-dts";
+import postcssImport from 'postcss-import';
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
-      {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: "esm",
-        sourcemap: true,
-      },
+      {file: packageJson.main, format: 'cjs', sourcemap: true, inlineDynamicImports: true},
+      {file: packageJson.module, format: 'es', sourcemap: true, inlineDynamicImports: true}
     ],
-    inlineDynamicImports: true,
     plugins: [
+      peerDepsExternal(),
+      commonjs(),
+      nodePolyfills(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      json(),
       postcss({
         sourceMap: true,
         extract: true,
         minimize: true,
         plugins: [postcssImport()]
       }),
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      json(),
-      typescript({ tsconfig: "./tsconfig.json" }),
       svgr(),
-      terser(),
+      terser()
     ],
-    external: ["react", "react-dom", "styled-components"],
+    watch: {
+      clearScreen: false
+    }
   },
   {
     input: "src/index.ts",
-    output: [{ file: "dist/types.d.ts", format: "es" }],
+    output: [{ file: "dist/index.d.ts", format: "es" }],
     plugins: [dts()],
-  },
+    external: [/\.css$/], // telling rollup anything that is .css aren't part of type exports 
+  }
 ];
