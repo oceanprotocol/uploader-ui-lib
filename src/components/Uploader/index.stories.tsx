0c7faeccce56bb8e4783057f6eff171fb80ed51f
Enzo Vezzaro <enzo-vezzaro@live.it>
2023-10-06 09:43:15 +0200

    restore env variables

--- src/components/Uploader/index.stories.tsx
+++ src/components/Uploader/index.stories.tsx
@@ -17,8 +17,8 @@ const Template: StoryFn<typeof Uploader> = (args) => <Uploader {...args} />;
 
 export const UploaderArgs = Template.bind({});
 UploaderArgs.args = { 
-    uploader_url: process.env.DBS_URL, 
-    uploader_account: process.env.DBS_ACCOUNT,
+    uploader_url: process.env.UPLOADER_URL, 
+    uploader_account: process.env.UPLOADER_ACCOUNT,
     infuraId: process.env.PUBLIC_INFURA_PROJECT_ID,
     walletConnectProjectId: process.env.PUBLIC_WALLETCONNECT_PROJECT_ID,
 };
