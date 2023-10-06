8fec341dd758ea2da928150b490ac1ab8fd9030a
Jamie Hewitt <jamie.hewitt15@gmail.com>
2023-10-06 14:00:21 +0300

    Fixing balance check on mumbai

--- src/@utils/checkBalance.ts
+++ src/@utils/checkBalance.ts
@@ -1,17 +1,16 @@
-import { useAccount, useContractRead } from 'wagmi';
-
-const WMATIC_ADDRESS = "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270";
+import { useAccount, useContractRead, useNetwork } from 'wagmi';
 
 export async function checkBalance(): Promise<any> {
     try {
         const { address } = useAccount()
+        const { chain } = useNetwork()
         
         // Get the ERC20 contract instance for WMATIC
         const minERC20Abi = [
             'function balanceOf(address owner) view returns (uint256)'
         ];
         const { data } = useContractRead({
-            address: WMATIC_ADDRESS,
+            address: chain?.id === 80001 ? '0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889' : '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
             abi: minERC20Abi,
             functionName: 'balanceOf',
             args: [address]
